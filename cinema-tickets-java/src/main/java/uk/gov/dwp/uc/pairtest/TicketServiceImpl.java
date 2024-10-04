87b14bb77db19bd003fc6fd25140d33cd72bfbcb
Sneha Grace Alex <snehaalexgrace@gmail.com>
2024-10-04 12:23:51 +0100

    Update TicketServiceImpl.java
    implementation of TicketServiceImpl.java with objective, business rules, constraints & assumptions
    
    Signed-off-by: Sneha Grace Alex <snehaalexgrace@gmail.com>

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,16 +1,72 @@
 package uk.gov.dwp.uc.pairtest;
 
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
-
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.paymentgateway.TicketPaymentServiceImpl;
+import thirdparty.seatbooking.SeatReservationService;
+import thirdparty.seatbooking.SeatReservationServiceImpl;
 public class TicketServiceImpl implements TicketService {
-    /**
+   
+	/**
      * Should only have private methods other than the one below.
      */
 
+	private final TicketPaymentService ticketPaymentService;
+    private final SeatReservationService seatReservationService;
+
+    public TicketServiceImpl(TicketPaymentService paymentService, SeatReservationService reservationService) {
+        this.ticketPaymentService = paymentService;
+        this.seatReservationService = reservationService;
+    }    
     @Override
-    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests)throws InvalidPurchaseException {
+    	if (ticketTypeRequests == null || ticketTypeRequests.length == 0) {
+            throw new InvalidPurchaseException("At least one ticket must be purchased.");
+        }
+    	// Validate accountId
+        if (accountId <= 0) {
+            throw new InvalidPurchaseException("Invalid account ID");
+        }
 
+        int totalTickets = 0;
+        int adultTickets = 0;
+        int totalCost = 0;
+        int seatsToReserve = 0;
+
+        for (TicketTypeRequest request : ticketTypeRequests) {
+            int quantity = request.getNoOfTickets();
+            totalTickets += quantity;
+
+            switch (request.getTicketType()) {
+                case ADULT:
+                    totalCost += quantity * 25;
+                    seatsToReserve += quantity;
+                    adultTickets += quantity;
+                    break;
+                case CHILD:
+                    totalCost += quantity * 15;
+                    seatsToReserve += quantity;
+                    break;
+                case INFANT:
+                    // Infants don't cost and don't reserve seats
+                    break;
+            }
+        }
+
+        // Check if more than 25 tickets
+        if (totalTickets > 25) {
+            throw new InvalidPurchaseException("Cannot purchase more than 25 tickets");
+        }
+
+        // Ensure there are adult tickets if infants or children are purchased
+        if (adultTickets == 0 && totalTickets > 0) {
+            throw new InvalidPurchaseException("At least one adult ticket must be purchased");
+        }
+
+        // Process payment and reserve seats
+        ticketPaymentService.makePayment(accountId, totalCost);
+        seatReservationService.reserveSeat(accountId, seatsToReserve);
     }
-
 }
